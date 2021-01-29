e5ed016b6bc6954095a53454cf2549936625e6e2
권혁준 <khjgogle@gmail.com>
2021-01-29 22:48:56 +0900

    좀비코드

--- Assets/code/Player.cs
+++ Assets/code/Player.cs
@@ -26,9 +26,9 @@ public class Player : MonoBehaviour
 	SpriteRenderer SpriteRenderer2d;
 	Rigidbody2D rigid;
 
-	Vector3 movement;
+	Vector3 movement; 
 
-	private void Awake()
+	private void Awake() //sssss
     {
         animator = GetComponent<Animator>();
         animator.SetBool("sit?", false);
