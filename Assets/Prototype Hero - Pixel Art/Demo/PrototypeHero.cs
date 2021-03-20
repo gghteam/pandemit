f9b13cbad103a5fa26b701e0f02b6f109abdd56b
DESKTOP-OHD6DA9\USER <moonkanghyuck@gmail.com>
2021-03-20 11:54:02 +0900

    울프의 함수 이름들 첫글자 대문자로 바꿈. 울프가 사정거리 안에 들어왔을 때 가까우면 달리고 멀면 걸어서 접근함. 울프의 사정거리에서 벗어났을 때 냄새를 맡는 애니메이션이 나옴.

--- Assets/Prototype Hero - Pixel Art/Demo/PrototypeHero.cs
+++ Assets/Prototype Hero - Pixel Art/Demo/PrototypeHero.cs
@@ -327,7 +327,7 @@ public class PrototypeHero : MonoBehaviour
                 foreach (Collider2D collider in collider2Ds)
                 {
                     if (collider.tag == "monster"){
-                        collider.GetComponent<wolf>().hit(damage);
+                        collider.GetComponent<wolf>().Hit(damage);
                         GameObject hello = Instantiate (dil);
                         hello.transform.position=(collider.transform.position+new Vector3(Random.Range(-0.2f,0.2f),Random.Range(-0.2f,0.2f),0));
                         hello.GetComponent<damage>().damagechk = damage;
