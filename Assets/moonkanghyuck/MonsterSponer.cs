bd74d9df2aef5685da278291607b87bc234ecd16
DESKTOP-OHD6DA9\USER <moonkanghyuck@gmail.com>
2021-04-07 16:22:01 +0900

    몬스터 스포너 수정

--- Assets/moonkanghyuck/MonsterSponer.cs
+++ Assets/moonkanghyuck/MonsterSponer.cs
@@ -5,25 +5,34 @@ using UnityEngine;
 public class MonsterSponer : MonoBehaviour
 {
     [SerializeField]
-    List<GameObject> monsters = new List<GameObject> { };
+    List<GameObject> monsters = new List<GameObject> { }; // 사용하기 전에 크기와 몬스터프리펩을 넣어줘야함
 
     [SerializeField]
-    Transform[] transforms;
+    Transform[] transforms; // 사용하기 전에 크기 설정해줘야함
 
     // Start is called before the first frame update
-    void Start()
+    void Start() // 시작할 때 트랜스폼을 찾아서 사용함
     {
-        for(int i = 0;i < transforms.Length; i++)
+        for(int i = 0; i < transforms.Length;i++)
         {
-            int random = Random.Range(0, monsters.Count);
-            Instantiate(monsters[random], transforms[i]);
-            Debug.Log("소환");
+            transforms[i] = transform.GetChild(i);
         }
+       
+        Spone();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Spone() // 소환 함수
+    {
+        for (int i = 0; i < transforms.Length; i++)
+        {
+            int random = Random.Range(0, monsters.Count);
+            Instantiate(monsters[random], transforms[i].position, Quaternion.identity);
+        }
     }
 }
